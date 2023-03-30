196d993d403060749d2b72050acd5f2305857ba3
Ömer Islamoglu <trinsyca@gmail.com>
2023-03-31 00:11:34 +0300

    php öğrenim

--- PHP-ogrenim/o1.php
+++ PHP-ogrenim/o1.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Kelime Bul</title>
+    <title>Ay Seçimi | TrinsyCa</title>
+    <link rel="shortcut icon" href="php-icon.jpg">
 </head>
 <body>
     <?php $aylar = array("Ocak","Şubat","Ahmet","Nazif","Hikmet","Emre","Yılmaz","Ömer","Ahmet","Nazif","Hikmet","Aralık");

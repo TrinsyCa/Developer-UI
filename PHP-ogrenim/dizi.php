afa58b66231248fffc66e7dc616791b080cab52f
Ömer Islamoglu <trinsyca@gmail.com>
2023-03-30 22:24:36 +0300

    php-ogrenim

---
+++ PHP-ogrenim/dizi.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Diziler</title>
+</head>
+<body>
+    <?php
+    $aylar=array("Ocak","Şubat","Mart","Nisan","Mayıs","Haziran","Temmuz","Ağustos","Eylül","Ekim","Kasım","Aralık",
+    "Ocak","Şubat","Mart","Nisan","Mayıs","Haziran","Temmuz","Ağustos","Eylül","Ekim","Kasım","Aralık");
+    for($i=0;$i<12;$i++){
+        echo $aylar[$i]."<br>";
+    }
+    echo "<br>----------------------------<br>";
+
+    foreach($aylar as $x){ // veritabanından veri çekerken sıklıkla kulanılır.
+        echo $x."<br>";  //Dongu her dönüşünde dizinin elemanı $x değişkenine aktarır.
+    }
+    ?>
+</body>
+</html>
---
+++ PHP-ogrenim/ikiboyutludiziler.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $rastgele=rand(0,10);
+    $notlar=array("Ali"=>rand(0,100),"Yağmur"=>rand(0,100),"Ege"=>rand(0,100),"Fırat"=>rand(0,100),"Erkan"=>rand(0,100),);
+
+    foreach($notlar as $isim=>$not)   //Dongu her dönüşünde dizini elemanı $isim ve $not değişkenlerine aktarır.
+    {
+        echo $isim."-".$not."<br>";
+    }
+    ?>
+</body>
+</html>
